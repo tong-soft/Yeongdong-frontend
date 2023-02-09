8788912b2a520f93c9673d8d83ea41e88baa5de9
euncherry <csmo2642@naver.com>
2023-02-09 15:01:59 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import YoungDongRouter from './router';
 import run from './init/start';
-import { React, useEffect } from 'react';
+import { React, useEffect, useState } from 'react';
 import 'antd/dist/reset.css';
 
 function App() {
+  const [isInit, setIsInit] = useState(false);
+
   useEffect(() => {
-    run();
+    run().then(() => {
+      setIsInit(true);
+    });
   }, []);
-  return (
+
+  return isInit ? (
     <>
       <YoungDongRouter />
     </>
-  );
+  ) : null;
 }
 
 export default App;
