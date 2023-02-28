78403eb467a3ce09827a04b01deffb6fff453657
euncherry <csmo2642@naver.com>
2023-02-28 12:15:15 +0900

    :rocket: env production

--- src/config/env.js
+++ src/config/env.js
@@ -8,11 +8,11 @@ export default env === 'development'
       ..._,
       REDIRECT_URL: _.DEV_REDIRECT_URL,
       LOGOUT_REDIRECT_URL: _.DEV_LOGOUT_REDIRECT_URL,
-      HOST_URL: _.DEV_HOST_URL,
+      // HOST_URL: _.DEV_HOST_URL,
       CLIENT_ID: _.DEV_CLIENT_ID,
       CALLBACK_URL: _.DEV_CALLBACK_URL,
       CLIENT_SECRET: _.CLIENT_SECRET,
-      SERVER_URL: _.DEV_SERVER_URL,
+      // SERVER_URL: _.DEV_SERVER_URL,
       BASE_URL: _.DEV_BASE_URL,
     }
   : {
